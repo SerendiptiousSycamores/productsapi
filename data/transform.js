b970fb78686491092362cb285b03fea72afdc2fb
Ika Ogeil <nahlaogeil@gmail.com>
2021-03-20 16:05:01 -0400

    added indexes to tables

--- data/transform.js
+++ data/transform.js
@@ -1,22 +1,20 @@
 const db = require('./postgres.js')
 
 /* TRANSFORMATION */
-
-/* product data transformed in excel */
-
-/* transforms features table -- remove 1109284 rows with value of null */
+/* Product data transformed in excel */
+/* Features table -- remove 1109284 rows with value of null */
 var query = "DELETE FROM features WHERE value='null'";
 
-/* transforms features table -- correct spelling in 46275 rows with value of striaght */
+/* Features table -- correct spelling in 46275 rows with value of striaght */
 var query = "UPDATE features SET value='Straight' WHERE value='Striaght'";
 
-/* transforms skus table -- check skus that duplicate sizes for the same style */
+/* Skus table -- check skus that duplicate sizes for the same style */
 var query = "SELECT * FROM skus A INNER JOIN (SELECT style_id, size FROM skus GROUP BY style_id, size HAVING COUNT(*) > 1) B ON  A.style_id = B.style_id AND A.size = B.size"
 
-/* transforms skus table -- remove one duplicate size for same style */
+/* Skus table -- remove one duplicate size for same style */
 var query = "DELETE FROM skus WHERE sku_id=5";
 
-/* transforms related table -- remove rows where related ids equal current product id */
+/* Related table -- remove rows where related ids equal current product id */
 var query = "DELETE FROM related WHERE related_id=product_id"
 
 db.query(query, (err, result) => {
@@ -28,7 +26,12 @@ db.query(query, (err, result) => {
   }
 })
 
-
+/* INDEXING */
+CREATE INDEX product_id_index ON related (product_id);
+CREATE INDEX product_id_index_f ON features (product_id);
+CREATE INDEX product_id_index_s ON style (product_id);
+CREATE INDEX style_id_index_p ON photos (style_id);
+CREATE INDEX style_id_index_s ON skus (style_id);
 /* LOADER COMMANDS */
 -- COPY product(product_id, name, slogan, description, category, default_price)
 -- FROM './product.csv'
